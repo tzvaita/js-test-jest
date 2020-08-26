f38b234ea480530fa38a695c641ef60216613332
Ramesh <rameshnaidu.allu@gmail.com>
2020-08-26 20:08:35 +0530

    add all four basic operation test cases and methods for calculator

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -1,5 +1,17 @@
 import calculator from '../jsfiles/calculator';
 
-test('Object contains methods', () => {
+test('Calculator object contains add method', () => {
   expect(calculator.add(2, 5)).toBe(7);
+});
+
+test('Calculator object contains subtract method', () => {
+  expect(calculator.subtract(5, 1)).toBe(4);
+});
+
+test('Calculator object contains divide method', () => {
+  expect(calculator.divide(12, 3)).toBe(4);
+});
+
+test('Calculator object contains multiply method', () => {
+  expect(calculator.multiply(2, 5)).toBe(10);
 });
