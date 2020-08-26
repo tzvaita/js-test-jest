000d067e4cda0a3426205a5c2e481203c03c6c01
Ramesh <rameshnaidu.allu@gmail.com>
2020-08-26 21:08:31 +0530

    add function for array analysis

--- src/jsfiles/arrayAnalysis.js
+++ src/jsfiles/arrayAnalysis.js
@@ -1,9 +1,11 @@
 const analyze = (arr) => {
-
-  let obj = {
-    
-  }
-  return arr;
+  const obj = {
+    average: arr.reduce((a, b) => a + b) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    len: arr.length,
+  };
+  return obj;
 };
 
 export default analyze;
