f30cef97aac83dad9e02ce7045c71e1417a8ddec
Ramesh <rameshnaidu.allu@gmail.com>
2020-08-27 16:09:42 +0530

    fix caesar cipher function

--- src/jsfiles/caesar.js
+++ src/jsfiles/caesar.js
@@ -1,14 +1,12 @@
-const caesar = (str) => {
-  function cipher(e) {
-    if (/^[a-zA-Z]/.test(e)) {
-      if (e.charCodeAt(0) === 90 || e.charCodeAt(0) === 122) {
-        return String.fromCharCode(e.charCodeAt(0) + 1 - 26);
-      }
-      return String.fromCharCode(e.charCodeAt(0) + 1);
-    }
-    return e;
+const encode = (e, shift) => {
+  if (/^[a-zA-Z]/.test(e)) {
+    const charNo = e.charCodeAt(0);
+    const charShift = charNo >= 65 && charNo <= 90 ? 65 : 97;
+    return String.fromCharCode(((charNo - charShift + shift) % 26) + charShift);
   }
-  return str.split('').map(cipher).join('');
+  return e;
 };
+
+const caesar = (str, shift) => str.split('').map((char) => encode(char, shift)).join('');
 
 export default caesar;
